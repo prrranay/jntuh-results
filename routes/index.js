c4174ef7c56fd162c7c94b1a69314ef3d1721501
prrranay <pkd4876308@gmail.com>
2024-04-22 22:50:55 +0530

    add /2-1

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 router.get("/", (req, res) => {
   res.render("index");
 });
+router.get("/2-1", (req, res) => {
+  res.render("2-1");
+});
 router.get("/2-2", (req, res) => {
   res.render("2-2");
 });
