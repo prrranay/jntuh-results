82fa1803c0a65d869edc678eda0dacfd81cbd56f
prrranay <pkd4876308@gmail.com>
2024-05-17 17:53:39 +0530

    add navbar component

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index",{title:"Academic"});
 });
 router.get("/2-1", (req, res) => {
-  res.render("2-1");
+  res.render("2-1",{title:"2-1"});
 });
 router.get("/2-2", (req, res) => {
-  res.render("2-2");
+  res.render("2-2",{title:"2-2"});
 });
 router.get("/3-1", (req, res) => {
-  res.render("3-1");
+  res.render("3-1",{title:"3-1"});
 });
 
 module.exports = router;
